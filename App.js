e0cf142a23b2e9f00d612e3b1cb2c8682891464a
timotheeodushina <hsp20tim@gmail.com>
2024-01-05 19:23:32 -0700

    added codes

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//set up REDUX
+
+
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Let us build a new uber app</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
